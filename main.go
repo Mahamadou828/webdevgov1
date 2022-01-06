fd4a6b5059026de6f4e9380ef8f56a9fe63ba2c2
Mahamadou828 <opmadou@gmail.com>
2022-01-06 20:44:44 +0300

    feat(simpleserver): setting the response header

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 * @description it's the function who is call each type someone access our api
  */
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome class</h1>")
-	fmt.Println(*r)
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-
+	fmt.Println("Up And Serve!")
 	http.ListenAndServe(":3000", nil)
 }
