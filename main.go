f41c3cb6b3c826b7507d089d3a623103487eef23
Mahamadou828 <opmadou@gmail.com>
2022-01-06 20:54:38 +0300

    feat(simple server): adding a simple router and a support page

--- main.go
+++ main.go
@@ -10,7 +10,14 @@ import (
  */
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome class</h1>")
+	path := r.URL.RequestURI()
+
+	switch path {
+	case "/support":
+		fmt.Fprint(w, "To get in touch send a email to <a href=\"mailto:opmadou@gmail.com\"/>Support me!</a>")
+	default:
+		fmt.Fprint(w, "<h1>Welcome to my awesome class</h1>")
+	}
 }
 
 func main() {
